e6de71c211adeaf504a618d74af03a4a417c928a
wade98 <lolewade@gmail.com>
2025-12-05 10:46:23 -0300

    Refactor Docker deployment

--- startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python -c "from warera.api import update_gear_prices_from_api, update_food_and_ammo_from_api; update_gear_prices_from_api(); update_food_and_ammo_from_api()"
-gunicorn warera.app:app
